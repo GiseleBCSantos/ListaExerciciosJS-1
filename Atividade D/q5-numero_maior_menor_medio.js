a06df8377a345cc1b0130f175c7f3a03eea24442
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-02-24 14:50:37 -0300

    refatoração de codigo

--- Atividade D/q5-numero_maior_menor_medio.js
+++ Atividade D/q5-numero_maior_menor_medio.js
@@ -1,9 +1,9 @@
 import { get_number } from "./io.utils.js";
 
 function main(){
-    const numero1 = get_number('Insira o primeiro número: ')
-    const numero2 = get_number('Insira o segundo número: ')
-    const numero3 = get_number('Insira o terceiro número: ')
+    const numero1 = get_number('Insira o primeiro numero: ')
+    const numero2 = get_number('Insira o segundo numero: ')
+    const numero3 = get_number('Insira o terceiro numero: ')
 
     const maior = maior_numero(numero1, numero2, numero3)
     const medio = numero_medio(numero1, numero2, numero3)
@@ -47,11 +47,11 @@ function menor_numero(number1, number2, number3){
 
 
 function numero_medio(number1, number2, number3){
-    if ((number1 === maior_numero(number1, number2, number3)  || number1 === menor_numero(number1, number2, number3)) && (number2 === maior_numero(number1, number2, number3)  || number2 === menor_numero(number1, number2, number3))){
+    if (number3 > number2 && number1 > number3){
         return number3
     }
 
-    else if ((number2 === maior_numero(number1, number2, number3)  || number2 === menor_numero(number1, number2, number3)) && (number3 === maior_numero(number1, number2, number3)  || number3 === menor_numero(number1, number2, number3))){
+    else if (number2 > number1 && number1 > number3 || number3 > number1 && number1 > number2){
         return number1
     }
     else{

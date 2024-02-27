b266ce820138846edaaeb6fcce020c09e82d2049
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-02-27 10:52:57 -0300

    questão 24 iniciada

---
+++ Atividade F/q24.js
@@ -0,0 +1,36 @@
+import {get_number} from './io.utils.js';
+
+function main(){
+    console.log(">>>>Resolução de equação de 2° grau <<<<")
+
+    const coef_a = get_number('Insira o valor do coeficiente a: ')
+    const coef_b = get_number('Insira o valor do coeficiente b: ')
+    const coef_c = get_number('Insira o valor do coeficiente c: ')
+
+    const delta = calcular_delta(coef_a, coef_b, coef_c)
+    let raizes = calcular_raizes(coef_a, coef_b, delta)
+
+    console.log(`
+    O resultado da equação com os coeficientes 
+    > a = ${coef_a} 
+    > b = ${coef_b}
+    > c = ${coef_c}
+    
+    > Delta = ${delta}
+    É igual a:
+    >> ${raizes}`)
+}
+
+
+function calcular_delta(a, b, c){
+    return b**2 -1*(4*a*c)
+}
+
+
+function calcular_raizes(a, b, delta){
+    let raiz1 = (-1*b + delta**0.5) / 2*a
+    let raiz2 = (-1*b -1*(delta**0.5)) / 2*a
+    return `raiz 1 é igual a ${raiz1} e a raiz 2 é igual a ${raiz2}`
+}
+
+main()

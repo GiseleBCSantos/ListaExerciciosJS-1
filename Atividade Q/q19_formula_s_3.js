ec5d3f815b9f4cce17b7149ef12c3068a83233ac
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-04-14 10:03:19 -0300

    questao 19 feita

---
+++ Atividade Q/q19_formula_s_3.js
@@ -0,0 +1,28 @@
+import { get_integer_number } from "../io.utils.js";
+
+function main(){
+    const valor_n = get_integer_number('Insira um valor para N: ')
+
+    const valor_s = calcular_formula_s(valor_n)
+
+    console.log(`Se substituirmos N por ${valor_n} na formula de S, o resultado é igual à ${valor_s.toFixed(2)}.`)
+}
+
+function calcular_formula_s(n){
+    let resultado = 0
+    let numero = 1
+    let posicao = 1
+    for (let i = n; i >= 1; i--){
+        if (posicao % 2 === 1){
+            resultado += numero/i
+        }
+        else{
+            resultado -= i/numero
+        }
+        numero++
+        posicao++
+    }
+    return resultado
+}
+
+main()

a3f9c68bad2e890f0b55742100831a3682d96374
Gisele <you@example.com>
2024-09-10 11:59:54 -0300

    :recycle: modularizaçao e loop com while

--- Atividade W/torre_hanoi.js
+++ Atividade W/torre_hanoi.js
@@ -1,33 +1,39 @@
-import { get_text, print } from "../io.utils.js"
-import { torreCompleta, fill_towers, show_menu, receber_operacao , mover, limpar_tela, press_enter_to_continue, get_quantidade_jogadas, get_campeao, run_game, choose_level} from "./utils_hanoi.js"
+import { get_number_in_range, get_text, print } from "../io.utils.js"
+import { fill_towers, limpar_tela, press_enter_to_continue, get_quantidade_jogadas, get_campeao, run_game, choose_level, get_nome_valido} from "./utils_hanoi.js"
 
 function main(){
-    const torreR = []
-    const torreG = []
-    const torreB = []
+    let jogo_on = true
 
-    let operacoes_jogador_1 = []
-    let operacoes_jogador_2 = []
+    while (jogo_on){
+        const torreR = []
+        const torreG = []
+        const torreB = []
+    
+        let operacoes_jogador_1 = []
+        let operacoes_jogador_2 = []
+    
+        let nivel = choose_level()
+    
+        fill_towers(torreR, torreG, torreB, nivel)
+    
+        let nome_jogador1 = get_nome_valido("Qual seu nome? ")
+        run_game([...torreR], [...torreG], [...torreB], operacoes_jogador_1, nome_jogador1)
+        press_enter_to_continue()
+    
+        limpar_tela()
+        let nome_jogador2 = get_nome_valido("Qual seu nome? ")
+        run_game([...torreR], [...torreG], [...torreB], operacoes_jogador_2, nome_jogador2)
+        press_enter_to_continue()
+    
+        limpar_tela()
+        let ganhador = get_campeao(get_quantidade_jogadas(operacoes_jogador_1), nome_jogador1, get_quantidade_jogadas(operacoes_jogador_2), nome_jogador2)
+        print(ganhador)
 
-    let nivel = choose_level()
-
-    fill_towers(torreR, torreG, torreB, nivel)
-
-    let nome_jogador1 = get_text("Qual seu nome? ")
-    run_game([...torreR], [...torreG], [...torreB], operacoes_jogador_1, nome_jogador1)
-    let qnt_jogadas_jogador1 = get_quantidade_jogadas(operacoes_jogador_1)
-    print(`${nome_jogador1} ganhou em ${qnt_jogadas_jogador1} jogadas.`)
-    press_enter_to_continue()
-
-    limpar_tela()
-    let nome_jogador2 = get_text("Qual seu nome? ")
-    run_game([...torreR], [...torreG], [...torreB], operacoes_jogador_2, nome_jogador2)
-    let qnt_jogadas_jogador2 = get_quantidade_jogadas(operacoes_jogador_2)
-    print(`${nome_jogador2} ganhou em ${qnt_jogadas_jogador2} jogadas.`)
-    press_enter_to_continue()
-
-    let ganhador = get_campeao(qnt_jogadas_jogador1, nome_jogador1, qnt_jogadas_jogador2, nome_jogador2)
-    print(ganhador)
+        let reiniciar_jogo = get_number_in_range(1, 2, "Deseja jogar novamente? (1 - Sim) (2 - Nao) \n>> ")
+        if (reiniciar_jogo === 2){
+            jogo_on = false
+        }
+    }
 
 
 
